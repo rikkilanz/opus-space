3aa7d9cb4dbc14bd76c1f4602ced39804ca3be04
Rikki Soriano <rikkilanz@gmail.com>
2023-11-07 14:38:53 -0800

    Header styling

--- app/components/global/Header.jsx
+++ app/components/global/Header.jsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-import { brandLogoHorizontalWhite } from "@/assets";
+import Link from "next/link";
+import { brandLogoHorizontalWhite } from "../../assets/index";
 
 export default function Header() {
   return (
-    <header>
-      <Image src={brandLogoHorizontalWhite} alt="" />
-      <nav></nav>
+    <header className="flex justify-between items-center max-w-[1440px] mx-auto py-6 px-4 font-opensauce text-[1em]">
+      <Image className="max-w-[150px]" src={brandLogoHorizontalWhite} alt="" />
+      <nav>
+        <ul className="flex gap-[2em] items-center">
+          <li><Link href={'#'}>Service</Link></li>
+          <li><Link href={'#'}>Team</Link></li>
+          <li><Link href={'#'}>Projects</Link></li>
+        </ul>
+      </nav>
+      <Link href={'#'} className="bg-primary py-4 px-8 rounded-full">
+        Contact Us
+      </Link>
     </header>
   );
 }
