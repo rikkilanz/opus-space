3efe8ed790c42bb3cb39b7a0694a7d8bf89f65af
Rikki Soriano <rikkilanz@gmail.com>
2023-11-08 16:09:22 -0800

    Header component. Responsive UI styling added.

--- app/components/global/Header.jsx
+++ app/components/global/Header.jsx
@@ -1,16 +1,64 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { brandLogoHorizontalWhite } from "../../assets/index";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  const handleMenuButton = (e) => {
+    setMenuOpen(e);
+  };
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+    setIsDesktop(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsDesktop(event.matches);
+      // Set mobile menu off if screen changes to desktop
+      if (!isDesktop) {
+        setMenuOpen(false);
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <header className="flex justify-between items-center max-w-[1440px] mx-auto py-6 px-4 font-opensauce text-[1em] sticky top-0 w-full lg:bg-transparent bg-black">
       <Image
-        className="max-w-[150px] order-1 lg:mr-[30%]"
+        className="max-w-[150px] order-1"
         src={brandLogoHorizontalWhite}
         alt="opus space logo in white with opus space text wordmark"
       />
+      {/* Check if screen is laptop size. If true, display this and centered */}
+      {isDesktop && (
+        <nav className="bg-transparent order-2 hidden lg:block">
+          <XMarkIcon className="h-10 w-10 lg:hidden mt-8 mr-8 cursor-pointer" />
+          <ul
+            className="flex lg:gap-[2em] flex-col lg:flex-row w-full"
+            onClick={() => handleMenuButton(false)}
+          >
+            <li className="hover:border-b-2 py-6 px-8 lg:p-0">
+              <Link href={"#"}>Service</Link>
+            </li>
+            <li className="hover:border-b-2 py-6 px-8 lg:p-0">
+              <Link href={"#"}>Team</Link>
+            </li>
+            <li className="hover:border-b-2 py-6 px-8 lg:p-0">
+              <Link href={"#"}>Projects</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
       <div className="flex items-center gap-[1em] order-2 lg:order-3">
         <Link
           href={"#"}
@@ -18,22 +66,31 @@ export default function Header() {
         >
           Contact Us
         </Link>
-        <Bars3Icon className="h-10 w-10 p-1 lg:hidden cursor-pointer" />
+        <Bars3Icon
+          className="h-10 w-10 p-1 lg:hidden cursor-pointer"
+          onClick={() => handleMenuButton(true)}
+        />
       </div>
-      <nav className="flex flex-col items-end lg:block bg-white lg:bg-transparent absolute lg:relative top-0 right-0 lg:order-2 lg:w-full h-screen lg:h-[24px] w-[60vw] sm:w-[40vw] text-right text-black lg:text-white">
-        <XMarkIcon className="h-10 w-10 lg:hidden mt-8 mr-8" />
-        <ul className="flex lg:gap-[2em] flex-col lg:flex-row w-full">
-          <li className="hover:border-b-2 py-6 px-8 lg:p-0">
-            <Link href={"#"}>Service</Link>
-          </li>
-          <li className="hover:border-b-2 py-6 px-8 lg:p-0">
-            <Link href={"#"}>Team</Link>
-          </li>
-          <li className="hover:border-b-2 py-6 px-8 lg:p-0">
-            <Link href={"#"}>Projects</Link>
-          </li>
-        </ul>
-      </nav>
+      {/* Mobile navigation slider. Appears when screen is mobile AND if menu button is clicked*/}
+      {menuOpen && !isDesktop && (
+        <nav className="flex flex-col items-end bg-white absolute lg:hidden top-0 right-0 h-screen w-[60vw] sm:w-[40vw] text-right text-black">
+          <XMarkIcon
+            className="h-10 w-10 lg:hidden mt-8 mr-4 cursor-pointer"
+            onClick={() => handleMenuButton(false)}
+          />
+          <ul className="flex lg:gap-[2em] flex-col lg:flex-row w-full">
+            <li className="hover:border-b-2 py-4 px-8 lg:p-0">
+              <Link href={"#"}>Service</Link>
+            </li>
+            <li className="hover:border-b-2 py-4 px-8 lg:p-0">
+              <Link href={"#"}>Team</Link>
+            </li>
+            <li className="hover:border-b-2 py-4 px-8 lg:p-0">
+              <Link href={"#"}>Projects</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
---
+++ app/helpers/checkBreakpoint.js
@@ -0,0 +1,16 @@
+export default function checkBreakpoint() {
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 671px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+}
