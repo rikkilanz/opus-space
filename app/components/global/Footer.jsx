d0ee61a482aaa8cea56b2f5322e77ca4419c25bf
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2023-11-18 21:56:18 -0800

    Changes made to gap, font weight, and removed a few things

--- app/components/global/Footer.jsx
+++ app/components/global/Footer.jsx
@@ -9,28 +9,25 @@ export default function Footer() {
             <div className="max-w-[1440px] mx-auto flex flex-row justify-evenly mt-20 mb-20">
                 <div className="">
                     <Image 
-                        className="w-[190px] mb-3"
+                        className="w-[190px] mb-[24px]"
                         src={brandLogoHorizontalWhite}
                     />
-                    <h3 className="font-epilogue text-[35px] w-[500px]">Would you like to work with us? Let's work together.</h3>
+                    <h3 className="font-epilogue font-semibold text-[35px] w-[500px]">Would you like to work with us? Let's work together.</h3>
                     <Link className="bg-primary py-4 px-6 rounded-full w-[150px] text-center font-epilogue block mt-[20px]" href={'#'}>Get Started</Link>
                 </div>
-                <div className="w-[180px]">
+                {/* <div className="w-[180px]">
                     <h3 className="font-epilogue text-[30px] mb-[16px]">Studio</h3>
                     <p className="font-epilogue">555 Seymour St, Vancouver, BC V6B 3HB</p>
                     <p className="font-epilogue mt-[12px]">+1 604 - 222 - 2211</p>
-                </div>
+                </div> */}
                 <div className="flex flex-col">
-                    <div className="mb-[16px]">
-                        <h3 className="font-epilogue text-[30px]">Connect</h3>
+                    <div className="mb-[24px]">
+                        <h3 className="font-epilogue font-semibold text-[30px]">Connect</h3>
                     </div>
                     <div className="flex flex-wrap gap-5 w-[300px]">
                         <Link href={'#'} className="underline font-epilogue">Instagram</Link>
-                        <Link href={'#'} className="underline font-epilogue">Facebook</Link>
                         <Link href={'#'} className="underline font-epilogue">LinkedIn</Link>
-                        <Link href={'#'} className="underline font-epilogue">Behance</Link>
-                        <Link href={'#'} className="underline font-epilogue">Dribble</Link>
-                        <Link href={'#'} className="underline font-epilogue">hello@opusspace.ca</Link>
+                        <Link href={'#'} className="underline font-epilogue">Email us at: hello@opusspace.ca</Link>
                     </div>
                 </div>
             </div>
