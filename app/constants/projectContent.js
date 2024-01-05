3abbf91485d3ffb875d59dc57fe2287c485ce2ae
Rikki Soriano <rikkilanz@gmail.com>
2024-01-04 19:33:29 -0800

    Content constant js added

---
+++ app/constants/projectContent.js
@@ -0,0 +1,38 @@
+export const projectContent = [
+    {
+        id: 1,
+        name: "Forward | Meet and Connect with Local Mentors",
+        tags: ["UIUX", "Web Development"],
+        blurb: "Discover meaningful mentorship connections in your local community with Forward, a web application dedicated to fostering knowledge-sharing and skill development.",
+        description: "Forward is a web platform designed to seamlessly connect individuals seeking guidance with local mentors. Whether you're delving into UI/UX or navigating the world of web development, Forward facilitates meaningful mentorship connections, allowing knowledge and skills to flourish.",
+        projectUrl: "https://forward.marlowecheng.com/public/",
+        imageContent: []
+    },
+    {
+        id: 2,
+        name: "Pizza Coming Soon",
+        tags: ["Graphic Design", "Branding"],
+        blurb: "A Slice of Perfection: Pizza Coming Soon Fresh Menu Redesign",
+        description: "",
+        projectUrl: "https://forward.marlowecheng.com/public/",
+        imageContent: []
+    },
+    {
+        id: 3,
+        name: "JJewelry Mobile eCommerce Site",
+        tags: ["UIUX"],
+        blurb: "",
+        description: "",
+        projectUrl: "",
+        imageContent: []
+    },
+    {
+        id: 4,
+        name: "Live 2K | Music Festival Website",
+        tags: ["UIUX", "Web Development"],
+        blurb: "Live 2K is a 2000's theme music festival, featuring famous artists from the 2000's era. The website is designed to be a one-stop shop for all things Live 2K.",
+        description: "Live 2K website is designed to be a one-stop shop for all things Live 2K. The website features ticket purchasing, artist lineup, and more.",
+        projectUrl: "https://live2k.rikkisoriano.com/",
+        imageContent: []
+    },
+]
