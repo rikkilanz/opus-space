ff95122839c88d5fdf9ad4725d7e86b9d066e7a0
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2024-01-25 01:23:12 -0800

    added image to be clickable

--- app/components/sections/Projects.jsx
+++ app/components/sections/Projects.jsx
@@ -17,27 +17,29 @@ export default function Projects() {
       <div className="grid grid-cols-1 lg:grid-cols-2">
         {projectContent.map((project) => {
           return (
-            <div className="group relative" key={project.id}>
-              <Image
-                className="w-full group-hover:bg-slate-900 group-hover:opacity-50 cursor-pointer"
-                src={project.imageContent[0]}
-                alt="project thumbnail for forward 2023"
-              />
-              <Link href="/project">Hello</Link>
-              <div className="absolute font-opensaucebold text-white font-bold inset-0 ml-2 opacity-0 group-hover:opacity-100 md:text-[36px] lg:text-[40px]">
-                {project.name}
+            <Link key={project.id} href="/project">
+              <div className="group relative">
+                  <Image
+                    className="w-full group-hover:bg-slate-900 group-hover:opacity-50 cursor-pointer"
+                    src={project.imageContent[0]}
+                    alt="project thumbnail for forward 2023"
+                  />
+                <div className="absolute font-opensaucebold text-white font-bold inset-0 ml-2 opacity-0 group-hover:opacity-100 md:text-[36px] lg:text-[40px]">
+                  {project.name}
+                </div>
+                <div className="absolute font-opensauceitalic text-white bottom-0 right-0 mr-2 opacity-0 group-hover:opacity-100 md:text-[22px] lg:text-[34px]">
+                  {project.tags.map((tag, index) => {
+                    return (
+                      <span key={tag}>
+                        {tag}
+                        {index !== project.tags.length - 1 && " | "}
+                      </span>
+                    );
+                  })}
+                </div>
               </div>
-              <div className="absolute font-opensauceitalic text-white bottom-0 right-0 mr-2 opacity-0 group-hover:opacity-100 md:text-[22px] lg:text-[34px]">
-                {project.tags.map((tag, index) => {
-                  return (
-                    <span key={tag}>
-                      {tag}
-                      {index !== project.tags.length - 1 && " | "}
-                    </span>
-                  );
-                })}
-              </div>
-            </div>
+            </Link>
+
           );
         })}
       </div>
