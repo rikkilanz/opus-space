89ae8a389d78a59ce0953918eb59568e26656c87
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2024-01-30 01:07:43 -0800

    Changes to 404 page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+import React from "react";
+import { useRouter } from "next/navigation";
 import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
 import CTACarousel from "./components/sections/CTACarousel"
@@ -5,10 +8,17 @@ import Services from "./components/sections/Services";
 import OurTeam from "./components/sections/OurTeam";
 import Projects from "./components/sections/Projects";
 import Footer from "./components/global/Footer";
-import Error from "./components/sections/404page.jsx";
+import NotFoundPage from "./components/global/not-found.jsx";
 import Contact from "./components/sections/Contact";
 
+
 export default function Home() {
+
+  const router = useRouter();
+
+  // Check if the router is not found 
+  
+
   return (
     <main className="antialiased">
       <Hero />
