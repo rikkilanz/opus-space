78cb7a7c05acf80aa0e59d34fea2d0c5baa01d99
Rikki Soriano <rikkilanz@gmail.com>
2024-02-03 12:50:32 -0800

    updated 404 styling

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import Logo3D from "./components/Logo3D";
 
 export default function NotFoundPage() {
   return (
-    <div className="flex flex-col items-center gap-5">
-      <h1 className="text-primary font-epilogue font-bold text-[40px]">
-        404: error
-      </h1>
+    <section id="hero-section">
+    <div className="max-w-[1440px] mx-auto pt-[50px] md:py-[50px] lg:pt-0 text-center">
+      <div className="relative">
+        <h1 className="text-[100px] sm:text-[120px] lg:text-[190px] 2xl:text-[227px] font-epilogue font-medium leading-[1em] tracking-[-0.05em] py-6 px-4">
+          404 Page
+        </h1>
+        <div className="z-[-2] absolute top-[9rem] right-0 left-0 mx-auto opacity-[50%] blur-xl orb" />
+      </div>
+        <div className="block absolute top-20 left-0 right-0 z-[-1] h-[500px] w-full">
+          <Logo3D />
+        </div>
+      <h2 className="font-opensauce text-[2.25em] leading-[1em] tracking-[-0.05em] py-6 px-4 mb-[32px]">
+        Sorry the page you were looking for doesn't exist!
+      </h2>
       <Link
-        href={"/"}
-        className="leading-[1em] tracking-[-0.05em] bg-primary hover:bg-[#424FFF] duration-[2] transition-all ease-in-out font-epilogue hover:font-bold py-4 px-6 rounded-full m-6 lg:m-0 text-center lg:mt-[20px] flex items-center"
+        href="/"
+        className="bg-primary mx-4 py-6 px-8 rounded-full w-[170px] text-center font-epilogue hover:font-bold hover:bg-[#424FFF] duration-[2] transition-all ease-in-out"
       >
-        Return to home
+        Go Home
       </Link>
     </div>
+    {/* <div className="z-[-1] h-[164px] bg-gradient-to-t from-[rgba(199,70,231,1)] to-[rgba(10,6,218,0)]" /> */}
+  </section>
   );
 }
