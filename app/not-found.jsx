f840c7a87338a89f94274a2c47d215d236ab073b
Rikki Soriano <rikkilanz@gmail.com>
2024-02-03 12:43:54 -0800

    404 page added

---
+++ app/not-found.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center gap-5">
+      <h1 className="text-primary font-epilogue font-bold text-[40px]">
+        404: error
+      </h1>
+      <Link
+        href={"/"}
+        className="leading-[1em] tracking-[-0.05em] bg-primary hover:bg-[#424FFF] duration-[2] transition-all ease-in-out font-epilogue hover:font-bold py-4 px-6 rounded-full m-6 lg:m-0 text-center lg:mt-[20px] flex items-center"
+      >
+        Return to home
+      </Link>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,22 @@
-'use client'
-import React, {useEffect} from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
-import CTACarousel from "./components/sections/CTACarousel"
+import CTACarousel from "./components/sections/CTACarousel";
 import Services from "./components/sections/Services";
 import OurTeam from "./components/sections/OurTeam";
 import Projects from "./components/sections/Projects";
-import NotFoundPage from "./components/global/NotFoundPage";
-import Footer from "./components/global/Footer";
 import Contact from "./components/sections/Contact";
 
-
 export default function Home() {
-
-  // const router = useRouter();
-
-  // useEffect(() => {
-
-  //   const { asPath } = router;
-
-  //   // Check if the route is not found or is still being generated
-  //   if (router.isFallback || typeof window !== 'undefined' && !router.asPath.startsWith('/v')) {
-  //     // Redirect to the 404 page
-  //     router.replace("/not-found");
-  //   }
-  // }, [router.isFallback, router.asPath]);
-
   return (
     <main className="antialiased">
       <Hero />
       <About />
       <CTACarousel />
-      <Services/>
-      <NotFoundPage/>
-      <OurTeam/>
-      <Projects/>   
-      <Contact/>
+      <Services />
+      <OurTeam />
+      <Projects />
+      <Contact />
     </main>
   );
 }
