a3385ac62afbc2f36b7b347955deaf2d99b05d3b
Rikki Soriano <rikkilanz@gmail.com>
2024-09-23 15:06:40 -0700

    Updated team section

--- app/components/sections/OurTeam.tsx
+++ app/components/sections/OurTeam.tsx
@@ -1,4 +1,3 @@
-import TeamSlider from "../TeamSlider";
 import { teamData } from "@/app/constants";
 import Image from "next/image";
 
@@ -15,13 +14,25 @@ export default function OurTeam() {
         {teamData.map((team) => {
           return (
             <li key={team.id} className="basis-[33%]">
-              <Image
-                height={500}
-                width={500}
-                className="rounded-md h-full w-auto grayscale hover:grayscale-0 transition-all blur-[1px] hover:blur-0"
-                src={team.imageUrl}
-                alt={`a profile photo of ${team.name}`}
-              />
+              <a
+                href={team.href}
+                target="_blank"
+                className="group relative overflow-hidden"
+              >
+                <Image
+                  height={500}
+                  width={500}
+                  className="rounded-md h-full w-auto grayscale group-hover:grayscale-0 transition-all blur-[1px] group-hover:blur-0"
+                  src={team.imageUrl}
+                  alt={`a profile photo of ${team.name}`}
+                />
+                <div className="absolute font-opensaucebold leading-[1em] tracking-[-0.05em] text-white font-bold inset-0 ml-2 mt-2 opacity-0 group-hover:opacity-100 md:text-[36px] lg:text-[40px] 2xl:text-[60px] uppercase transition-all ease-in-out duration-[2]">
+                  {team.name}
+                </div>
+                <div className="absolute font-opensauceitalic leading-[1em] tracking-[-0.05em] text-white bottom-0 right-0 mr-2  mb-2 opacity-0 group-hover:opacity-100 md:text-[22px] lg:text-[34px] 2xl:text-[34px] transition-all ease-in-out duration-[2]">
+                  {team.title}
+                </div>
+              </a>
             </li>
           );
         })}
